2e520169039f90911f103b505dbb70713ed34dce
user <user@DESKTOP-6AEADSB>
2022-04-27 15:37:04 +0900

    2차 추가

---
+++ mygit/src/main/java/com/web/controller/FaqController.java
@@ -0,0 +1,19 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FaqController {
+
+	@RequestMapping(value="/faq_list.do",method=RequestMethod.GET)
+	public String faqList() {
+		return "/faq/faq_list";
+	}
+	
+	@RequestMapping(value="/faq_write.do",method=RequestMethod.GET)
+	public String faqWrite() {
+		return "/faq/faq_write";
+	}
+}
