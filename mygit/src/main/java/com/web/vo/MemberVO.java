09944ad06ce8caa75e910fa79e7a324ad2b88939
Kim <Kim@Kim-PC>
2022-05-20 11:49:47 +0900

    -- 로그인 관련

---
+++ mygit/src/main/java/com/web/vo/MemberVO.java
@@ -0,0 +1,91 @@
+package com.web.vo;
+
+public class MemberVO {
+	String id, pw, name, gender, email, address, phone, joindate;
+	String email1, email2, ph1, ph2, ph3;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getJoindate() {
+		return joindate;
+	}
+	public void setJoindate(String joindate) {
+		this.joindate = joindate;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		if (email==null) {
+			email = String.join("@", email1, email2);
+		}
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		if(phone == null) {
+			phone = String.join("-", ph1, ph2, ph3);
+		}
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail1() {
+		return email1;
+	}
+	public void setEmail1(String email1) {
+		this.email1 = email1;
+	}
+	public String getEmail2() {
+		return email2;
+	}
+	public void setEmail2(String email2) {
+		this.email2 = email2;
+	}
+	public String getPh1() {
+		return ph1;
+	}
+	public void setPh1(String ph1) {
+		this.ph1 = ph1;
+	}
+	public String getPh2() {
+		return ph2;
+	}
+	public void setPh2(String ph2) {
+		this.ph2 = ph2;
+	}
+	public String getPh3() {
+		return ph3;
+	}
+	public void setPh3(String ph3) {
+		this.ph3 = ph3;
+	}
+}
